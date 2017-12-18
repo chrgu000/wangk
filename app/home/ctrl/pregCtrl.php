2f28baabfaa9c1746a606003b403518b36f21f3b
wang3kai@163.com <wk900208>
2017-12-18 09:04:59 +0800

    合并正则

--- app/home/ctrl/pregCtrl.php
+++ app/home/ctrl/pregCtrl.php
@@ -10,6 +10,47 @@ class PregCtrl extends Ctrl{
 
 	}
 
+	//测试元数据
+	public function unit(){
+		// $pattern = '/Windows\d+/'; 
+		// $str = "boWindowse221.com"; 
+		// $pattern = '/\b\w{6}\b/';
+	    //  $str = "bo Window .com"; 
+		$pattern = '/^\d{5,12}$/';
+	    $str = "1234777"; 
+	    preg_match($pattern, $str, $match); 
+	    dump($match);
+
+	}
+
+	//引用删除
+	public function yingy(){
+		echo "<br />";
+		$v3 = 'val';
+		$v4 = &$v3;
+		unset($v4);
+		var_dump($v3);
+	}
+
+	//得到系统已定义的变量
+	public function sysInfo(){
+		dump(get_defined_vars());
+		dump(get_defined_constants());
+	}
+
+	//预定义的变量
+	public function beforeDefine(){
+		dump(__CLASS__);
+		dump(__NAMESPACE__);
+	}
+
+	public function str(){
+		$s = 'hello\\world';
+		$s = "hi\r\nhhh";
+		dump($s);
+	}
+
+
 	
 }
 
