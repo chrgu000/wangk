f77a63550939bceb0582fcb142917d4819df8d5a
wang3kai@163.com <wk900208>
2017-12-20 17:25:43 +0800

    php、linux文件新增

---
+++ app/home/ctrl/huiCtrl.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\home\ctrl;
+use core\lib\ctrl;
+
+		
+//这是用来测试hui-ui的		
+class HuiCtrl extends Ctrl{
+
+	public function index(){
+		
+		$this->display('index');
+	}
+
+	
+
+	
+}
+
